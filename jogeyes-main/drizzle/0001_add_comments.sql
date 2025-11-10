54c1ef59b3b506c1ba5d2c2debd69d8d130ebf24
hartej <hartejs@outlook.com>
2025-11-09 19:20:01 -0800

    WE

--- jogeyes-main/drizzle/0001_add_comments.sql
+++
@@ -1,8 +0,0 @@
--- Migration: add comments table
-CREATE TABLE IF NOT EXISTS comment (
-  id TEXT PRIMARY KEY,
-  post_id TEXT NOT NULL,
-  name TEXT NOT NULL,
-  text TEXT NOT NULL,
-  created_at INTEGER NOT NULL
-);
--- jogeyes-main/src/data/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./writing";
-export * from "./videos";
-export * from "./music";
-export * from "./photography";
-
-// Central exports for data files. Import from '@/data' to get posts, videos, tracks, and photography samples.
